da318d3284f8bed274e6ecd4dcd67e14831925a3
SENNIA Othmane <155919972+Zaza9216@users.noreply.github.com>
2025-01-21 16:11:27 +0100

    new changes

--- app.py
+++ app.py
@@ -1,30 +1,31 @@
-from flask import Flask, render_template, request, jsonify
+from Flask import Flask, render_template, request, jsonify, redirect, url_for
 
 app = Flask(__name__)
 
 books = [
-{"id": 1, "title": "Harry Potter and the Sorcerer's Stone", "author": "J.K. Rowling", "year": 1997},
-{"id": 2, "title": "The Great Gatsby", "author": "F. Scott Fitzgerald", "year": 1925},
-{"id": 3, "title": "1984", "author": "George Orwell", "year": 1949},
-{"id": 4, "title": "To Kill a Mockingbird", "author": "Harper Lee", "year": 1960},
-{"id": 5, "title": "A Promised Land", "author": "Barack Obama", "year": 2020}
+    {"id": 1, "title": "Harry Potter and the Sorcerer's Stone", "author": "J.K. Rowling", "year": 1997},
+    {"id": 2, "title": "The Great Gatsby", "author": "F. Scott Fitzgerald", "year": 1925},
+    {"id": 3, "title": "1984", "author": "George Orwell", "year": 1949},
+    {"id": 4, "title": "To Kill a Mockingbird", "author": "Harper Lee", "year": 1960},
+    {"id": 5, "title": "A Promised Land", "author": "Barack Obama", "year": 2020}
 ]
 
-@app.route('/', methods=["GET", "POST"])
+@app.route('/hetic', methods=['GET', 'POST'])
 def index():
-    return render_template("index.html")
+    return render_template('index.html')
 
-@app.route('/api/v1/qui-suis-je', methods=["GET", "POST"])
+@app.route('/quisuisje', methods=['GET'])
 def qui_suis_je():
-    return "Je suis Othmane!"
+    return 'othmane'
 
-@app.route('/api/v1/qui-suis-je/<name>', methods=["GET", "POST"])
-def getNameDynamic (name):
-    return name
+@app.route('/quisuisjev2/<name>', methods=['GET', 'POST'])
+def dynamic_qui_suis_je(name):
+    return f'Bonjour, {name}! 🔥'
 
-@app.route('/api/v1/addition/<a>/<b>', methods=["GET", "POST"])
-def addition (a,b):
-    return str(int(a)+int(b))
+@app.route('/api/v1/addition/<a>/<b>', methods=['GET'])
+def add(a, b):
+    c = int(a) + int(b)
+    return str(c)
 
 @app.route('/api/v1/books', methods=['GET'])
 def get_books_by_author():
@@ -45,9 +46,18 @@ def search_form():
             return render_template('search_results.html', error="Aucun livre trouvé pour cet auteur.")
     return render_template('search.html')
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8080)
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        # Vous pouvez ajouter ici la logique de vérification du login et du mot de passe
+        if username == 'admin' and password == 'password':  # Exemple de vérification simple
+            return redirect(url_for('index'))
+        else:
+            error = 'Nom d\'utilisateur ou mot de passe incorrect'
+            return render_template('login.html', error=error)
+    return render_template('login.html')
 
-
-
-
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80)
--- dockerfile
+++ dockerfile
@@ -1,13 +1,13 @@
 FROM python:3.3
 
-WORKDIR /serveurdataIA2
+WORKDIR /DataAPI
 
-COPY . /serveurdataIA2/
+COPY . /DataAPI
 
-RUN pip install --no-cache-dir -r requirements.txt
+RUN pip install --no-cache-dir --trusted-host pypi.org --trusted-host pypi.python.org --trusted-host files.pythonhosted.org -r requirements.txt
 
 EXPOSE 80
 
 ENV NAME World
 
-CMD ["python", "app.py"]
+CMD ["python3", "app.py"]
